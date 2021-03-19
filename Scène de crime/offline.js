fa3ee50e7452e90f0f4a3ed9fb40e269ef01869c
Vert2vent <glansartdelessan@gmail.com>
2021-03-19 14:00:27 +0100

    Scène de crime

---
+++ Scène de crime/offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1616158742,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/fenêtrecadavre-sheet0.png",
+		"images/indicecadavre-sheet0.png",
+		"images/indicecadavre-sheet1.png",
+		"images/indicecadavre-sheet2.png",
+		"images/fenêtrepleinelune-sheet0.png",
+		"images/fenêtreforêt-sheet0.png",
+		"images/fenêtreempreintes-sheet0.png",
+		"images/fenêtreÉclats-sheet0.png",
+		"images/fenêtretorche-sheet0.png",
+		"images/fenêtreoffrandes-sheet0.png",
+		"images/fond-sheet0.png",
+		"images/cadre-sheet0.png",
+		"media/331375__xanco123__mysterious-event-music-01.m4a",
+		"media/331375__xanco123__mysterious-event-music-01.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
