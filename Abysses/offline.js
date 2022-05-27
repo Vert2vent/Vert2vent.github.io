1dd11d356c44f43091b5c4280a610aa415142172
Vert2vent <glansartdelessan@gmail.com>
2022-05-27 23:37:42 +0200

    gervais

---
+++ Abysses/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1653687414,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/fond-sheet0.png",
+		"images/poisson-sheet0.png",
+		"images/poisson2-sheet0.png",
+		"images/poisson3-sheet0.png",
+		"images/poisson4-sheet0.png",
+		"images/poisson5-sheet0.png",
+		"images/poisson6-sheet0.png",
+		"images/poisson7-sheet0.png",
+		"images/titre-sheet0.png",
+		"images/bulle-sheet0.png",
+		"images/bulle2-sheet0.png",
+		"images/ombre-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
